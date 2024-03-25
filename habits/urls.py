e9607c2026cae05225588a48c02feb7b5291f0d8
RikoshetGames <ilyalew@mail.ru>
2024-03-26 00:00:17 +0300

    CourseWorkDRF: validators, paginations, permissions

--- habits/urls.py
+++ habits/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from habits.apps import HabitsConfig
 from habits.views.habits import HabitListAPIView, HabitDetailAPIView, HabitCreateAPIView, HabitUpdateAPIView, \
-    HabitDeleteAPIView
+    HabitDeleteAPIView, HabitPublicListAPIView
 
 from habits.views.nice_habits import NiceHabitListAPIView, NiceHabitDetailAPIView, NiceHabitCreateAPIView, \
     NiceHabitUpdateAPIView, NiceHabitDeleteAPIView
@@ -15,6 +15,8 @@ urlpatterns = [
     path('habits/create/', HabitCreateAPIView.as_view(), name='habits_create'),
     path('habits/update/<int:pk>/', HabitUpdateAPIView.as_view(), name='habits_update'),
     path('habits/delete/<int:pk>/', HabitDeleteAPIView.as_view(), name='habits_delete'),
+
+    path('public_habits/', HabitPublicListAPIView.as_view(), name='public_habits_list'),
 
     path('nice_habits/', NiceHabitListAPIView.as_view(), name='habits_list'),
     path('nice_habits/<int:pk>/', NiceHabitDetailAPIView.as_view(), name='habits_detail'),
