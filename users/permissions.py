a0de2d2f310879635db7cd0108af3d14bac5d01a
RikoshetGames <ilyalew@mail.ru>
2024-03-26 22:28:37 +0300

    CourseWorkDRF: permission update

--- users/permissions.py
+++ users/permissions.py
@@ -6,4 +6,4 @@ class IsOwner(BasePermission):
     message = "Вы не являетесь владельцем"
 
     def has_object_permission(self, request, view, obj):
-        return request.user == obj.user
+        return request.user == obj.owner
