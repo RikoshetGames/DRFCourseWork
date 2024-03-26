b42b0381a30d795de4c6da60bbae9b4d0d2a67c5
RikoshetGames <ilyalew@mail.ru>
2024-03-26 22:24:10 +0300

    CourseWorkDRF: urls names

--- habits/urls.py
+++ habits/urls.py
@@ -18,9 +18,9 @@ urlpatterns = [
 
     path('public_habits/', HabitPublicListAPIView.as_view(), name='public_habits_list'),
 
-    path('nice_habits/', NiceHabitListAPIView.as_view(), name='habits_list'),
-    path('nice_habits/<int:pk>/', NiceHabitDetailAPIView.as_view(), name='habits_detail'),
-    path('nice_habits/create/', NiceHabitCreateAPIView.as_view(), name='habits_create'),
-    path('nice_habits/update/<int:pk>/', NiceHabitUpdateAPIView.as_view(), name='habits_update'),
-    path('nice_habits/delete/<int:pk>/', NiceHabitDeleteAPIView.as_view(), name='habits_delete'),
+    path('nice_habits/', NiceHabitListAPIView.as_view(), name='nice_habits_list'),
+    path('nice_habits/<int:pk>/', NiceHabitDetailAPIView.as_view(), name='nice_habits_detail'),
+    path('nice_habits/create/', NiceHabitCreateAPIView.as_view(), name='nice_habits_create'),
+    path('nice_habits/update/<int:pk>/', NiceHabitUpdateAPIView.as_view(), name='nice_habits_update'),
+    path('nice_habits/delete/<int:pk>/', NiceHabitDeleteAPIView.as_view(), name='nice_habits_delete'),
 ]
